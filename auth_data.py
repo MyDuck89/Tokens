84d5b7b8a21fa1ca2317894f08ddba7d48423467
Alexey <93479033+MyDuck89@users.noreply.github.com>
2023-06-10 12:01:47 +0300

    Add token JBB

--- auth_data.py
+++ auth_data.py
@@ -3,3 +3,6 @@ token = '5029479269:AAE7QDKJtsZFmnVBdbBMpjM42WTAYmxNmfc'
 
 # Tinkoff API
 tinkoff_token = 't.KZke0GpNxsti75jPN7As28i5waKoUy23e6v6hxNP8haNCzNxB8GWugPZFqQg4ZFJZutkZVMIPnF9lAHI_ff1cQ'
+
+# James_Bonds_bot
+token_JBB = '6112134948:AAH6x_alVeRYjGDEC8YPg1B6BsN_CBnA9j8'
