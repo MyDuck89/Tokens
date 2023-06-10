6599413ce5da086239d9a171f1c827d0872fc560
Alexey <93479033+MyDuck89@users.noreply.github.com>
2023-06-10 11:49:08 +0300

    Tokens for Bots

---
+++ auth_data.py
@@ -0,0 +1,5 @@
+# Bot for tests
+token = '5029479269:AAE7QDKJtsZFmnVBdbBMpjM42WTAYmxNmfc'
+
+# Tinkoff API
+tinkoff_token = 't.KZke0GpNxsti75jPN7As28i5waKoUy23e6v6hxNP8haNCzNxB8GWugPZFqQg4ZFJZutkZVMIPnF9lAHI_ff1cQ'
